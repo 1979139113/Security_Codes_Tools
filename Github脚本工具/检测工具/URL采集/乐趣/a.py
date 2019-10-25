44942ae26a2121e928bc0a067d181e75222903bd
kongxin520 <ikongxin@gmail.com>
2019-10-25 05:53:17 +0000

    delete folder

--- Github脚本工具/检测工具/URL采集/乐趣/a.py
+++
@@ -1,5 +0,0 @@
-# coding:utf-8
-a = list(set(x.replace('\n','') for x in open('1.txt','r').readlines()))
-import sys
-for x in a:
-    sys.stdout.write('|'+x)
--- Github脚本工具/检测工具/必应/debug2.py
+++
@@ -1,6 +0,0 @@
-import os
-dir = 'http'
-files= os.walk(dir)
-for files in os.walk(dir):
-    filename = files[2]
-print(filename)
